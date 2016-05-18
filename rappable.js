5b8385a0de19a549c4d6eab5065f175a2ee06730
Pascal Seitz <pascal.seitz@sap.com>
2016-05-18 08:48:03 +0200

    asdf

---
+++ rappable.js
@@ -0,0 +1,14 @@
+var metaphone = require('./rapmetaphone');
+
+var service = {};
+
+functions isRappable(word1, word2, lang){
+    if (!lang || lang = 'en') 
+    {
+        return metaphone.isGoodToRap(word1, word2);
+    }
+}
+
+service.isRappable = isRappable;
+service.metaphone = metaphone;
+module.exports = service;

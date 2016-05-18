e1ff1941011a0f3359831da7cdd6fbf0b0f6dc0e
Pascal Seitz <pascal.seitz@sap.com>
2016-05-18 08:54:09 +0200

    blubber

--- rappable.js
+++ rappable.js
@@ -3,7 +3,7 @@ var metaphone = require('./rapmetaphone');
 var service = {};
 
 function isRappable(word1, word2, lang){
-    if (!lang || lang = 'en') 
+    if (!lang || lang == 'en') 
     {
         return metaphone.isGoodToRap(word1, word2);
     }
